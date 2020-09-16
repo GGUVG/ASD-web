c82b5fcc0022953c379981de454f75b2102cc817
chency <www.ccygguvg@qq.com>
2020-09-16 11:58:37 +0800

    实现文章bgImg的显示，就是比较傻逼还要去另一个文件夹启动service.js，待优化

--- src/components/welfare/ArticleList.vue
+++ src/components/welfare/ArticleList.vue
@@ -2,23 +2,26 @@
     <Hello1>
     <template slot="middleContent">
         <template>
-        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <div slot="footer"><b>无圣光套图</b> GGUVG</div>
+        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="articleList">
+        <div slot="footer"><b>www.baidu.com</b> 百度一下,你就知道</div>
             <a-list-item slot="renderItem" key="item.title" slot-scope="item">
-            <template v-for="{ type, text } in actions" slot="actions">
+            <!-- <template v-for="{ type, text } in actions" slot="actions">
             <span :key="type">
             <a-icon :type="type" style="margin-right: 8px" />
             {{ text }}
             </span>
-            </template>
+            </template> -->
+            <!--解决浏览器Not allowed to load local resource 
+              https://blog.csdn.net/alazyperson/article/details/105905824?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.edu_weight&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.edu_weight 
+            -->
             <img
             slot="extra"
             width="272"
             alt="logo"
-            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />
-        <a-list-item-meta :description="item.description">
+            :src=item.bgImg />
+        <a-list-item-meta :description="item.secondaryTitle">
         <a slot="title" :href="item.href">{{ item.title }}</a>
-        <a-avatar slot="avatar" :src="item.avatar" />
+        <a-avatar slot="avatar" :src="avatarImg" />
         </a-list-item-meta>
         {{ item.content }}
         </a-list-item>
@@ -29,19 +32,9 @@
 </template>
 <script>
 const listData = []
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
 import Hello1 from '../Hello1'
 import {findArticleByPage,findWelfareImgByPage} from './WelfareService'
+import avatarBgImg from '../../assets/img/avatar/IMG_20180820_164059.jpg'
 export default {
 name:'newVue',
 components:{
@@ -53,19 +46,9 @@ mounted () {
 data () 
 {
     return{
-        listData,
-        pagination: {
-        onChange: page => {
-          console.log(page);
-        },
-        pageSize: 3,
-      },
-      actions: [
-        { type: 'star-o', text: '156' },
-        { type: 'like-o', text: '156' },
-        { type: 'message', text: '2' },
-      ],
+      listData,
       articleList:[],
+      avatarImg:avatarBgImg,
       criteria:{
 
       },
@@ -95,13 +78,17 @@ methods:{
             if(res.status==0)
             {
                 let ret=res.data.rows
-                // console.log('ret',ret)
+                console.log('ret',ret)
                 self.articleList=ret
-                self.$message.success('加载文章列表...')
-                // console.log('articleList:...',self.articleList)
+                self.articleList.map(val=>{
+                  val.bgImg = 'http://127.0.0.1:9092/welfare/' + val.bgImg
+                })
+                // self.$message.success('加载文章列表...')
+                console.log('articleList:...',self.articleList)
             }else
             {
                 self.$message.error('加载文章列表失败!'+res.message)
+                return
             }
         })
     }
